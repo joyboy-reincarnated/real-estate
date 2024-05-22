93a6c254f4e656dbe477de3543e12bb63173d188
vijayprakash2024 <vijayprakash@minnotra.com>
2024-05-22 09:53:33 +0530

    Complete show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,7 +9,7 @@ import {
 } from "firebase/storage";
 import { app } from "../firebase";
 import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserFailure, signOutUserStart, signOutUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/user/userSlice";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   const { currentUser ,loading, error} = useSelector((state) => state.user);
@@ -19,8 +19,9 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false)
+  const [showListingError, setShowListingError] = useState(false)
+  const [userListings, setUserListings] = useState([])
   const dispatch = useDispatch()
-  const navigate = useNavigate()
   console.log(file);
   console.log(filePerc);
   console.log(formData);
@@ -143,7 +144,21 @@ export default function Profile() {
       dispatch(signOutUserFailure(data.message))
     }
   }
-  console.log(formData)
+  const handleShowListings = async()=>{
+      try {
+        setShowListingError(false)
+        const res = await fetch(`/api/user/listings/${currentUser._id}`)
+        const data = await res.json()
+        if (data.success ===false) {
+          showListingError(true)
+          return
+        }
+        setUserListings(data)
+      } catch (error) {
+        setShowListingError(true)
+      }
+  } 
+   console.log(formData)
   return (
     <div className="p-3  max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center">Profile</h1>
@@ -201,7 +216,7 @@ export default function Profile() {
         </button>
         <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={'/create-listing'}>
           Create Listing
-        </Link>
+        </Link> 
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
@@ -211,6 +226,30 @@ export default function Profile() {
       <p className="text-green-700 mt-5">
         {updateSuccess ? 'User is updated successfully' : ''}
       </p>
+      <button className="text-green-700 w-full" onClick={handleShowListings}>Show Listings</button>
+      <p className="text-red-700 mt-5">
+        {showListingError ? 'Error  showing listings' : ''}
+      </p>
+      {
+        userListings && userListings.length>0 &&
+        <div className=" flex flex-col gap-4">
+            <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
+         { userListings.map((listing)=>(
+              <div className="border rounded-lg p-3 flex justify-between items-center gap-4" key={listing._id}>
+                <Link to={`/listing/${listing._id}`}>
+                  <img src={listing.imageUrls[0]} alt="Listing images" className="h-16 w-16 object-contain" />
+                </Link>
+                <Link  to={`/listing/${listing._id}`} className="text-slate-700 font-semibold flex-1 hover:underline truncate">
+                  <p >{listing.name} </p>
+                </Link> 
+                <div className="flex flex-col item-center">
+                  <button className="text-red-700"> Delete</button>
+                  <button className="text-green-700"> Edit</button>
+                </div>
+              </div>
+          ))}
+        </div> 
+      }
     </div>
   );
 }
